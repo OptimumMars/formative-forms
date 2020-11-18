9d4fcff4e7bd17b2722fb7eca5932d626f6948cf
bryant <kleinb1616@gmail.com>
2020-11-18 17:42:09 -0500

    finishing up phase 3

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const csrf = require('csurf');
 
 app.set("view engine", "pug");
 
-app.use(cookieParser())
+app.use(cookieParser());
+app.use(express.urlencoded());
 
 const csrfProtection = csrf({ cookie: true });
 
@@ -21,6 +22,36 @@ const users = [
   }
 ];
 
+const validate = (req, res, next) => {
+  const { firstName, lastName, email, password, confirmPassword } = req.body;
+ 
+  const errors = [];
+
+  if (!firstName) {
+    errors.push("Please provide a first name.");
+  }
+  
+  if(!lastName){
+    errors.push('Please provide a last name.');
+  }
+
+  if (!email) {
+    errors.push("Please provide an email.");
+  }
+
+  if(!password){
+    errors.push("Please provide a password.")
+  }
+
+  if(password !== confirmPassword){
+    errors.push("The provided values for the password and password confirmation fields did not match.");
+  }
+
+
+  req.errors = errors;
+  next();
+};
+
 app.get("/", (req, res) => {
   res.render('index', { users });
 });
@@ -29,7 +60,21 @@ app.get("/create", csrfProtection, (req, res) => {
   res.render("form", { csrfToken: req.csrfToken() })
 });
 
-// app.get("/create")
+app.post("/create", csrfProtection, validate, (req, res)=>{
+  const { firstName, lastName, email, password } = req.body;
+  //console.log(req.body)
+  if (req.errors.length > 0) {
+    res.render("form", {
+      firstName,
+      lastName,
+      email,
+      errors: req.errors,
+    })
+
+    users.push({firstName, lastName, email})
+   // res.redirect("/");
+}
+});
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
